ab327e9845f921eb8e73da570c52ac98e9ca1664
Adrian Statescu <mergesortv@gmail.com>
2013-02-23 18:44:24 -0800

    image src set default

--- lastfm.py
+++ lastfm.py
@@ -24,11 +24,15 @@ def get_recent_tracks(user):
     tracks = []
     for raw_track in raw_tracks:
         try:
+           imgsrc = raw_track['image'][0]["#text"]
+           if imgsrc == "":
+              imgsrc = "http://cdn.last.fm/flatness/catalogue/noimage/2/default_artist_small.png"
+           print imgsrc  
            track = {
                    'name': raw_track['name'],
                    'artist': raw_track['artist']['#text'],
                    'url': raw_track['url'],
-                   'image_url': raw_track['image'][0]['#text']  
+                   'image_url': imgsrc
                    }
            tracks.append(track)
         except Exception, e:
