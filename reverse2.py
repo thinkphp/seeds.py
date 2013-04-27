87eed9ccbcd7edb72265005bd1351ab2b6ef9fac
Adrian Statescu <mergesortv@gmail.com>
2013-04-26 23:20:45 -0700

    recursive solution

--- reverse2.py
+++ reverse2.py
@@ -5,6 +5,7 @@
 '''
 import sys
 
+#iterative version
 def reverse(n):
     s = 0
     x = n
@@ -13,12 +14,19 @@ def reverse(n):
           x = int(x/10)
     return s
 
+#recursive solution
+def reverse_rec(s,n):
+    if n == 0: 
+       return s
+    else:
+       return reverse_rec(s*10+n%10,int(n/10))  
+
 if __name__ == "__main__":
    if len(sys.argv) != 2:
-      print "Usage: python reverse2.py 123"
+      print "Usage: python reverse2.py 12345678"
       sys.exit()
    n = int(sys.argv[1])
-   print reverse(n)
+   print reverse_rec(0,n)
 
 
 
