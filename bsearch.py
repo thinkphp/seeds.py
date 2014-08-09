abaf916d693a28f7c25896bc8714262848f9e419
Adrian Statescu <mergesortv@gmail.com>
2014-08-09 09:07:11 -0700

    fixed some bugs and adding a new version solved recursively with li>ls

--- bsearch.py
+++ bsearch.py
@@ -1,34 +1,84 @@
-```
-    Binary Search
-```
+#Binary Search
 
 #iterative solution
 def binarySearch(what,arr):
+
     li = 0
     ls = len(arr)-1
+
     while li<=ls:
+
           middle = (li+ls)/2
+
           if(what == arr[middle]):
              return middle
+
           elif what > arr[middle]:
              li = middle + 1
           else:
-             ls = middle - 1    
+             ls = middle - 1
+
     return False
 
 #recursive solution
 def binarySearchRec(what,li,ls,arr):
+     
+    if li>ls:
+       return False
+  
     middle = (li+ls)/2
+
     if what == arr[middle]:
        return middle
+
     elif what > arr[middle]:
+
        return binarySearchRec(what,middle+1,ls,arr)
+
     else:
-       return binarySearchRec(what,li,middle-1,arr)
-    
+       return binarySearchRec(what,li,middle-1,arr) 
+
+#I'm using recursively solution as well
+def searchbin(arr, x):
+
+    return _searchbin(arr, x, 0, len(arr)-1)
+
+def _searchbin(arr, x, li, ls):
+
+    if li>ls: 
+       return -1
+
+    m = (li+ls)/2
+
+    if x == arr[m]:
+       return m 
+
+    if x > arr[m]: 
+       return _searchbin(arr,x,m+1,ls)
+
+    if x < arr[m]: 
+       return _searchbin(arr,x,li,m-1)    
+
 
 #main
 if __name__ == "__main__":
-     arr = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+
+     arr = [1,2,3,4,5,6,7]
+     search = 3
+
      #assert 19 == binarySearch(20,arr)
-     print binarySearchRec(1,0,len(arr),arr)
+
+     #first attempt
+     print binarySearch(2,arr)
+
+     #second attempt
+     print binarySearchRec(22,0,len(arr)-1,arr)
+
+     #third attempt
+     print arr
+     print 'I am searching...:', search
+     print 'Position:', searchbin(arr,search)
+
+
+    
+
