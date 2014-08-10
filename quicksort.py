862e61e9b62c6d5c8e3591092dffb7a68210b595
Adrian Statescu <mergesortv@gmail.com>
2014-08-10 01:20:29 -0700

    rename the file

--- quicksort.py
+++ quicksort.py
@@ -1,4 +1,4 @@
-#public class quicksort
+#Most discussions tent to end up with Quick Sort Algorithm
 class QuickSort:
 
   #define public vector to get array from input
@@ -7,58 +7,80 @@ class QuickSort:
   #defined length of the array
   len = -1
 
+  #A little variable on which we store the pivot of the vector and first time we set to -1
+  #to the left of the pivot p we have numbers less than p and to the right we have elements greater than p
+  p = -1
+
+  #constructor of the class 
   def __init__(self, arr):
 
         self.vec = arr
         self.len = len(self.vec)
-        self.sort() 
 
-
+  #public method getter
   def get(self):
       return self.vec  
 
+  #public method setter
   def set(self,arr):
-       self.vec = arr
+      self.vec = arr
 
+  #public method sort
   def sort(self):
       self._qs(0,self.len-1)
 
-  def _qs(self,li,ls):
-
-      p = self.partition(li,ls)
-      if p+1<ls:
-         self._qs(p+1,ls)
-      if p-1>li:
-         self._qs(li,p-1)
-
-          
-   
+  #private method finding the pivot
   def partition(self,li,ls):
 
-      pivot = self.vec[ls]
+      i = li
+      j = ls
+      i1 = 0
+      j1 = -1
+     
+      aux = 0
 
-      s = li - 1
-      
-      for i in range(li,ls+1):
+      while i<j:
 
-          if self.vec[i] <= pivot:
-             s = s + 1
-             self.swap(s,i)
+            if self.vec[i] > self.vec[j]:
 
-      return s
+               self._swap(i,j)  
 
-  def swap(self,a,b):
+               aux = i1
+               i1 = -j1
+               j1 = -aux
 
-      temp = self.vec[a]
-      self.vec[a] = self.vec[b]
-      self.vec[b] = temp  
-         
-         
+            i = i + i1
+            j = j + j1
+
+      self.p = i    
+
+
+
+  #private method doing divide et impera
+  def _qs(self,li,ls):
+
+    if li<ls:
+
+      self.partition(li,ls)  
+      self._qs(li,self.p-1) 
+      self._qs(self.p+1,ls)  
+
+
+  #private method additional helper function to interchange some elements from different positions
+  def _swap(self,i,j):
+      temp = self.vec[i]
+      self.vec[i] = self.vec[j]
+      self.vec[j] = temp   
  
+#let's take this vector as input
+arr=[9,8,7,6,5,4,3,2,1]
 
-arr=[9,8,7,6,5,4,3,2,1,-1,-3]
-
+#create an object of this class QuickSort
 ob = QuickSort(arr)
 
+#called sort method
+ob.sort()
+
+#and finally get the sorted array for quod erat demonstrandum
 print ob.get()       
 
