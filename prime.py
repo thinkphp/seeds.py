d741c306bda9c6bc9162576aa4a78dc8bdad2fcf
Adrian Statescu <mergesortv@gmail.com>
2017-03-26 19:42:58 -0700

    fixed some issues

--- prime.py
+++ prime.py
@@ -1,21 +1,35 @@
 import sys
+import math
 
-def isPrime(n):
+def isPrime( n ):
+
+    if(n == 1): return False
 
     if(n == 2): return True
 
-    for i in range(2,n):
+    if(n == 3): return True
+
+    for i in range(2, int(math.sqrt(n)) + 1):
+
         if(n % i == 0): 
+
            return False
+
         else:
+
            return True   
 
 if __name__ == "__main__":
+
      if len(sys.argv) != 2:
+
         print"Usage: python prime.py 1001"
         sys.exit()  
+
      n = int(sys.argv[1])  
+
      if isPrime(n):
+
         print "Is Prime"
      else:
         print "Not Prime"    
